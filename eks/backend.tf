47a92603d2d3b18972f0e6118b54119a03d4d216
Nitish Mishra <Nitish Mishra>
2025-01-20 09:38:59 +0530

    EKS terraform code added

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.9.3"
+  required_version = ">= 1.9.3"  # This removes the strict upper limit.
   required_providers {
     aws = {
       source  = "hashicorp/aws"
